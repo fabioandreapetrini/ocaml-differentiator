ec6fb2e680cbe9989140ff5bc9199876fa05a8e2
fabioandrea.petrini <fabioandrea.petrini@gmail.com>
2021-09-10 17:31:03 +0200

    rimozione commento input vecchio

--- derivaFunzioni.ml
+++ derivaFunzioni.ml
@@ -2,26 +2,6 @@ open Lexing
 open Parser
 open Deriv
 
-
-(*
-let rec elaboraRisposte lexbuf1 =
-  let result = Parser.main Lexer.token lexbuf1 in
-          Deriv.stampaEspressioneMath (Deriv.deriva result);
-          print_newline();
-          print_newline(); 
-          flush stdout;
-          (* print_string lexbuf1.lex_buffer; *)
-          elaboraRisposte lexbuf1
-
-let _ =
-  let filer = open_in Sys.argv.(1) in
-    let lexbuf1 = Lexing.from_channel filer in
-      elaboraRisposte lexbuf1;
-      close_in filer;
-      exit 0
-*)
-
-
 let espressioneInput = String.concat Sys.argv.(1) [""; "\n"];;
 
 let _ =
