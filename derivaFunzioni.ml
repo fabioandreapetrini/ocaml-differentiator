a3e29a7417d304d561609bd42d6aa62da9b65730
fabioandrea.petrini <fabioandrea.petrini@gmail.com>
2021-09-14 19:22:24 +0200

    gestione eccezione divizione per zero

--- derivaFunzioni.ml
+++ derivaFunzioni.ml
@@ -5,7 +5,9 @@ open Deriv
 let espressioneInput = String.concat Sys.argv.(1) [""; "\n"];;
 
 let _ =
-      let lexbuf = Lexing.from_string espressioneInput in
-      let result = Parser.main Lexer.token lexbuf in
-      Deriv.stampaEspressioneMath (Deriv.derivaEricombina result);
-
+      try
+            let lexbuf = Lexing.from_string espressioneInput in
+            let result = Parser.main Lexer.token lexbuf in
+            Deriv.stampaEspressioneMath (Deriv.derivaEricombina result);
+      with
+       | DivisionByZero -> print_string "Error_DivisionByZero";;
