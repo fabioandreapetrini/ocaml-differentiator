a0d5fe1ba5f8893f4ec34cc84584f1280145e64f
fabioandrea.petrini <fabioandrea.petrini@gmail.com>
2021-09-23 18:30:36 +0200

    fix espressione costante per espressione

--- derivaFunzioni.ml
+++ derivaFunzioni.ml
@@ -4,8 +4,12 @@ open Derivatore
 
 let espressioneInput = String.concat Sys.argv.(1) [""; "\n"];;
 
+
+ 
+
 let _ =
       try
+            (* Lexing.from_string crea buffer lexer che legge una stringa di caratteri *)
             let lexbuf = Lexing.from_string espressioneInput in
             let result = Parser.main Lexer.token lexbuf in
             Derivatore.stampaEspressione (Derivatore.derivaEricombina result);
